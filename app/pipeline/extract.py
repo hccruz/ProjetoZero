bc21a5ddececb02f49fa746f00f64dc6bd16c48c
Heraldo Candido da Cruz <hccruz@gmail.com>
2026-06-12 21:50:37 -0300

    update

--- app/pipeline/extract.py
+++ app/pipeline/extract.py
@@ -4,19 +4,19 @@ from typing import List
 
 import pandas as pd
 
-"""
-função para ler os arquivos de 
-uma pasta data/input e retornar uma lista de dataframes
-
-args: input_path (str): caminho da pasta com os arquivos
-
-return: lista de dataframes
-"""
 
 path = 'data/input'
 
 
 def extract_from_excel(path: str) -> List[pd.DataFrame]:
+    """
+    função para ler os arquivos de 
+    uma pasta data/input e retornar uma lista de dataframes
+
+    args: input_path (str): caminho da pasta com os arquivos
+
+    return: lista de dataframes
+    """
     all_files = glob.glob(os.path.join(path, '*.xlsx'))
 
     data_frame_list = []
